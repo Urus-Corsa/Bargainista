64933ed0f5a6e2f2a9b25e39d844dba082b0576a
Sina Kalantar <sina1999.sk@gmail.com>
2024-12-28 17:20:13 -0800

    Implemented reset stats, show stats buttons & utilized localStorage

--- js-scripts/04-rock-paper-scissors.js
+++ js-scripts/04-rock-paper-scissors.js
@@ -1,16 +1,28 @@
 let player = {
   move: undefined,
-  score: 0
+  score: parseInt(localStorage.getItem('playerScore'), 10) || 0
 };
 let computer = {
   move: undefined,
-  score: 0
+  'score': parseInt(localStorage.getItem('computerScore'), 10) || 0
 };
 let gameStats = {
-  // roundWinners: {
-  //   1: undefined,
-  // },
-  tiedRounds: 0
+  ['tiedRounds']: parseInt(localStorage.getItem('ties'), 10) || 0,
+  show: function showStats(){
+    const scoreBoard = `-------- SCORE BOARD --------\nYour Score: ${player.score}\nComputer's Score: ${computer.score}\nTied Rounds: ${gameStats.tiedRounds}`;
+    return scoreBoard;
+  },
+  reset: function resetGameStats(){ // functions stored inside of an object == methods
+    localStorage.clear()
+    localStorage.setItem('playerScore', '0');
+    localStorage.setItem('computerScore', '0');
+    localStorage.setItem('ties', '0');
+    // Reset in-memory objects
+    player.score = 0;
+    computer.score = 0;
+    this.tiedRounds = 0;
+    return 'All the game statistics and scores have been reset!';
+  }
 };
 
 function setComputersMove(){
@@ -20,56 +32,49 @@ function setComputersMove(){
   randomNumber < 1/3 ? 'rock' : 
   randomNumber < 2/3 ? 'paper' : 
   'scissors';
-  // return computersMove;
   computer.move = computersMove;
-
-  // if(0<=randomNumber && randomNumber<1/3){
-  //   console.log('computer picked rock');
-  //   return 'rock';
-  // } else if(1/3<=randomNumber && randomNumber<2/3){
-  //   console.log('computer picked paper');
-  //   return 'paper';
-  // } else {
-  //   console.log('computer picked scissors');
-  //   return 'scissors';
-  // }
 }
 
 function determineWinner(){
-  // let roundsPlayed =  Object.keys(gameStats.roundWinners).length;
   setComputersMove();
   let result = `Computer picked ${computer.move}.\n`;
   if(player.move === computer.move){
-    gameStats.tiedRounds ++;
+    gameStats.tiedRounds++;
+    localStorage.setItem('ties', (gameStats.tiedRounds).toString());
     result += 'It\'s a tie!';
   } else if (player.move === 'paper' && computer.move === 'rock' || player.move === 'rock' && computer.move === 'paper'){
     if(computer.move === 'paper'){
-      computer.score ++;
+      computer.score++;
+      localStorage.setItem('computerScore', (computer.score).toString());
       result += 'Computer wins!';
     }else{
-      player.score ++;
+      player.score++;
+      localStorage.setItem('playerScore', (player.score).toString());
       result += 'You win!';
     }
   } else if (player.move === 'paper' && computer.move === 'scissors' || player.move === 'scissors' && computer.move === 'paper'){
     if(computer.move === 'scissors'){
       computer.score ++;
+      localStorage.setItem('computerScore', (computer.score).toString());
       result += 'Computer wins!';
     }else{
-      player.score ++;
+      player.score++;
+      localStorage.setItem('playerScore', (player.score).toString());
       result += 'You win!';
     }
   } else if (player.move === 'rock' && computer.move === 'scissors' || player.move === 'scissors' && computer.move === 'rock'){
     if(computer.move === 'rock'){
-      computer.score ++;
+      computer.score++;
+      localStorage.setItem('computerScore', (computer.score).toString());
       result += 'Computer wins!';
     }else{
-      player.score ++;
+      player.score++;
+      localStorage.setItem('playerScore', (player.score).toString());
       result += 'You win!';
     }
   } else {
-    console.log(`An error has occurred in determineWinner()\nPlayer: ${player.move}\nComputer: ${computer.move}`);
-    return;
+    result = `An error has occurred in determineWinner()\nPlayer: ${player.move}\nComputer: ${computer.move}`;
   }
-  result += `\n-------- SCORE BOARD --------\nYour Score: ${player.score}\nComputer's Score: ${computer.score}\nTied Rounds: ${gameStats.tiedRounds}`;
+  result += `\n${gameStats.show()}`
   return result;
 }
