e87bf4d80a7ec4538ad9608f5c2599f7a0e2e37e
Sina Kalantar <sinakalantar09@gmail.com>
2024-12-30 19:52:54 -0800

    enhanced the live scoreboard and added last round results, current winner, last played moves, & BE code clean up

--- js-scripts/04-rock-paper-scissors.js
+++ js-scripts/04-rock-paper-scissors.js
@@ -3,105 +3,161 @@ const player = {
     upon initilization of player object as well as its 'move' property,
     we set it to //* null == intentionally want it to be empty rather than using 'undefined'
   */
-  move: null,
+  move: localStorage.getItem('playerMove') || '',
+  setMove(pickedMove){
+    this.move = pickedMove
+    localStorage.setItem('playerMove', pickedMove);
+  },
   score: parseInt(localStorage.getItem('playerScore'), 10) || 0,
   displayCurrentScore(){
-    document.querySelector('.js-game-stats-board .js-players-score').innerHTML = `Your Score: ${this.score}`;
+    document.querySelector('.js-stats-board .js-players-stats .js-players-score').innerText = `Score: ${this.score}`;
+  },
+  displayLastMove() {
+    document.querySelector('.js-stats-board .js-players-stats .js-players-last-move').innerText = `Last Played: ${this.move}`;
+  },
+  displayStats() {
+    this.displayCurrentScore()
+    this.displayLastMove()
   }
 };
 const computer = {
-  move: null,
+  move: localStorage.getItem('computerMove') || '',
+  setMove(){
+    const randomNumber = Math.random();
+    //* ternary operator
+    let computersMove = 
+    randomNumber < 1/3 ? 'rock' : 
+    randomNumber < 2/3 ? 'paper' : 
+    'scissors';
+    this.move = computersMove;
+    localStorage.setItem('computerMove', computersMove);
+  },
   'score': parseInt(localStorage.getItem('computerScore'), 10) || 0,
-  displayCurrentScore(){
-    document.querySelector('.js-game-stats-board .js-computers-score').innerHTML = `Computer's Score: ${this.score}`;
+  displayCurrentScore() {
+    document.querySelector('.js-stats-board .js-computers-stats .js-computers-score').innerText = `Score: ${this.score}`;
+  },
+  displayLastMove() {
+    document.querySelector('.js-stats-board .js-computers-stats .js-computers-last-move').innerText = `Last Played: ${this.move}`;
+  },
+  displayStats() {
+    this.displayCurrentScore()
+    this.displayLastMove()
   }
 };
 const gameStats = {
   ['tiedRounds']: parseInt(localStorage.getItem('ties'), 10) || 0,
+  lastRoundResult: localStorage.getItem('lastRoundResult') || '',
   // when explicitly defining a method, it can also be done using //* shorthand method shortcut -> display: function  displayGameStats() {} => displayGameStats() {}
-  displayGameStats() {
-    player.displayCurrentScore();
-    computer.displayCurrentScore();
-    document.querySelector('.js-game-stats-board .js-tied-rounds').innerHTML = `Tied Rounds: ${this.tiedRounds}`;
-    document.querySelector('.js-game-stats-board .js-total-rounds').innerHTML = `Total Rounds: ${player.score+computer.score+this.tiedRounds}`;
-    // const scoreboard = `-------- SCOREBOARD --------\nPlayer's Score: ${player.score}\nComputer's Score: ${computer.score}\nTied Rounds: ${this.tiedRounds}`;
-    // return scoreboard;
+  displayFrontRunner(){
+    const currentWinner = 
+    player.score > computer.score ? "You're in the lead!" :
+    player.score < computer.score ? "Computer's in the lead!" :
+    player.score === 0 && computer.score === 0 ? '' :
+    'Tie!'; 
+    document.querySelector('.js-stats-board .js-game-stats .js-front-runner').innerText = `Front Runnner: ${currentWinner}`;
+  },
+  displayTiedRounds() {
+    document.querySelector('.js-stats-board .js-game-stats .js-tied-rounds').innerText = `Tied Rounds: ${this.tiedRounds}`;
+  },
+  displayLastRoundResult(){
+    document.querySelector('.js-stats-board .js-game-stats .js-last-round-result').innerText = `Last Round's Result: ${this.lastRoundResult}`;
+  },
+  displayTotalRounds() {
+    document.querySelector('.js-stats-board .js-game-stats .js-total-rounds').innerText = `Total Rounds: ${player.score+computer.score+this.tiedRounds}`;
+  },
+  displayStats() {
+    this.displayFrontRunner();
+    this.displayLastRoundResult();
+    this.displayTiedRounds();
+    this.displayTotalRounds();
+  }
+};
+const scoreboard = {
+  display() {
+    player.displayStats();
+    computer.displayStats();
+    gameStats.displayStats();
   },
   reset: function resetGameStats(){ //* functions stored inside of an object == methods
     // reset local storage objects
     //! clearing the entire local storage objects may result in other stored data to be lost as well, localStorage.removeItem('key') can be used instead 
     //! localStorage.clear()
     localStorage.removeItem('playerScore');
+    localStorage.removeItem('playerMove');
+    
     localStorage.removeItem('computerScore');
+    localStorage.removeItem('computerMove');
+    
     localStorage.removeItem('ties');
+    localStorage.removeItem('lastRoundResult');
 
     // reset in memory objects
     player.score = 0;
+    player.move = '';
     computer.score = 0;
-    this.tiedRounds = 0;
+    computer.move = '';
+    gameStats.tiedRounds = 0;
+    gameStats.lastRoundResult = '';
     return 'All the game statistics and scores have been reset!';
   }
-};
-
-function setComputersMove(){
-  const randomNumber = Math.random();
-  //* ternary operator
-  let computersMove = 
-  randomNumber < 1/3 ? 'rock' : 
-  randomNumber < 2/3 ? 'paper' : 
-  'scissors';
-  computer.move = computersMove;
 }
 
 function determineWinner(){
-  setComputersMove();
+  computer.setMove();
   
   //*destructuring, can only be used if the variable name is the same as object's property name
   let { tiedRounds } = gameStats; 
-  let result = `Computer picked ${computer.move}.\n`;
+  let result = `Computer played ${computer.move}.\n`;
   if(player.move === computer.move){
     tiedRounds++;
+    gameStats.tiedRounds = tiedRounds;
+    gameStats.lastRoundResult = 'Was a tie!';
     /* 
       toString() is a method that belongs to an object that is being called on some integer value here, 
       which is tiedRounds in this case, JS wrappes this value/variable in a special object that has
       the toString() method == //* Auto-boxing featute of JS
     */
     localStorage.setItem('ties', tiedRounds.toString());
-    gameStats.tiedRounds = tiedRounds;
     result += 'It\'s a tie!';
   } else if (player.move === 'paper' && computer.move === 'rock' || player.move === 'rock' && computer.move === 'paper'){
     if(computer.move === 'paper'){
       computer.score++;
+      gameStats.lastRoundResult = 'Computer won!';
       localStorage.setItem('computerScore', computer.score.toString());
       result += 'Computer wins!';
     }else{
       player.score++;
+      gameStats.lastRoundResult = 'You won!';
       localStorage.setItem('playerScore', player.score.toString());
       result += 'You win!';
     }
   } else if (player.move === 'paper' && computer.move === 'scissors' || player.move === 'scissors' && computer.move === 'paper'){
     if(computer.move === 'scissors'){
       computer.score ++;
+      gameStats.lastRoundResult = 'Computer won!';
       localStorage.setItem('computerScore', computer.score.toString());
       result += 'Computer wins!';
     }else{
       player.score++;
+      gameStats.lastRoundResult = 'You won!';
       localStorage.setItem('playerScore', player.score.toString());
       result += 'You win!';
     }
   } else if (player.move === 'rock' && computer.move === 'scissors' || player.move === 'scissors' && computer.move === 'rock'){
     if(computer.move === 'rock'){
       computer.score++;
+      gameStats.lastRoundResult = 'Computer won!';
       localStorage.setItem('computerScore', computer.score.toString());
       result += 'Computer wins!';
     }else{
       player.score++;
+      gameStats.lastRoundResult = 'You won!';
       localStorage.setItem('playerScore', player.score.toString());
       result += 'You win!';
     }
   } else {
-    result = `An error has occurred in determineWinner()\nPlayer: ${player.move}\nComputer: ${computer.move}`;
+    return `An error has occurred in determineWinner()\nPlayer: ${player.move}\nComputer: ${computer.move}`;
   }
-  // result += `\n${gameStats.show()}`
+  localStorage.setItem('lastRoundResult', gameStats.lastRoundResult);
   return result;
 }
